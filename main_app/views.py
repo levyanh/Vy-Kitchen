55cbc9bf2ed9e2ce3e7dfd9ccdcabf251f913018
levyanh <levyanh1@gmail.com>
2021-04-06 11:35:15 -0700

    add recipe_detail

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
+from .models import Recipe
 # Create your views here.
 
 #Home view
@@ -14,7 +14,13 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-#About profile
+#Recipe_detail view
+def recipes_detail(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    return render(request, 'recipe_detail.html', context)
+
+
+#Profile view
 def profile(request):
     return render(request, 'profile.html')
 
