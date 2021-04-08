56978ecd9d0f8d6724e6ddb7828cb9a47ebd0d26
levyanh <levyanh1@gmail.com>
2021-04-07 20:28:19 -0700

    remove recipe_detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
     path('recipes/', views.home, name='recipes'),
-    # path('recipes/<int:recipe_id>/', views.recipe_detail, name="detail"),
+    # path('recipes/<int:recipe_id>/', views.recipe_detail, name="recipe_detail"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from .models import Profile, Recipe, Comment
 
 def home(request):
     recipes = Recipe.objects.all()
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'recipes': recipes})
 
 def about (request):
     return render(request, 'about.html')
@@ -12,6 +12,6 @@ def about (request):
 def profile (request):
     return render(request, 'profile.html')
 
-def recipe_detail (request):
-    recipes = get_object_or_404(Recipe, id=recipe_id)
-    return render(request, 'recipe_detail.html')
+# def recipe_detail(request, recipe_id):
+#     recipe = Recipe.objects.get(id=recipe_id)
+#     return render(request, 'recipe_detail.html', {'recipe': recipe})
